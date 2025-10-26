6ed05ba848c9a9ac8245d8e0c9b3f2e0471a7ed4
Dev Tec <devtec133127@gmail.com>
2025-10-26 14:04:56 +0100

    add CNAME for subdomain and small changes

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,7 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 8080,
   },
+  base: '/code-motion-hub/', 
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
     alias: {
